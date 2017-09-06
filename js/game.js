58a4dd325e35edb1b33542f70183ab17b664fa2f
MrZWH <1875290692@qq.com>
2017-09-06 17:07:04 +0800

    完成至计时

--- js/game.js
+++ js/game.js
@@ -1,6 +1,7 @@
 var Game =function() {
-    var gameDiv;
-    var nextDiv;
+    var gameDiv,
+        nextDiv,
+        timeDiv;
 
     var gameData = [
     [0,0,0,0,0,0,0,0,0,0],
@@ -48,12 +49,13 @@ var Game =function() {
             divs.push(Div);
         }
     };
+
     var refreshDiv = function(data,divs) {
         for(var  i = 0; i<data.length; i++) {
             for(var j= 0;j<data[0].length;j++) {
                 if(data[i][j] == 0) {
                     divs[i][j].className = 'none';
-                }else if(data[i][j] ==1) {
+                }else if(data[i][j] == 1) {
                     divs[i][j].className = 'done';
 
                 }else if(data[i][j] == 2) {
@@ -126,25 +128,130 @@ var Game =function() {
             cur.down();
             setData();
             refreshDiv(gameData,gameDivs);
+            return true;
+        } else {
+            return false;
         }
     };
 
+    // 右移
+    var right = function() {
+        if(cur.canRight(isValid)){
+            clearData();
+            cur.right();
+            setData();
+            refreshDiv(gameData,gameDivs);
+        }
+    };
+
+    // 左移
+    var left = function() {
+        if(cur.canLeft(isValid)){
+            clearData();
+            cur.left();
+            setData();
+            refreshDiv(gameData,gameDivs);
+        }
+    };
+    
+    // 旋转
+    var rotate = function() {
+        if(cur.canRotate(isValid)){
+            clearData();
+            cur.rotate();
+            setData();
+            refreshDiv(gameData,gameDivs);
+        }
+    };
+
+    // 方块移动到底部给它固定
+    var fixed = function() {
+        for(var i = 0; i < cur.data.length; i++) {
+            for(var j = 0; j < cur.data[0].length; j++) {
+                if(check(cur.origin, i, j)) {
+                    if(gameData[cur.origin.x + i][cur.origin.y + j] == 2) {
+                        gameData[cur.origin.x + i][cur.origin.y + j] = 1;
+                    };
+                };
+            };
+        };
+        refreshDiv(gameData, gameDivs);
+    };
+
+    // 消行
+    var checkClear = function() {
+        for(var i = gameData.length - 1; i >= 0; i--) {
+            var clear = true;
+            for(var j = 0; j < gameData[0].length; j++) {
+                if(gameData[i][j] != 1) {
+                    clear = false;
+                    break;
+                }
+            };
+            if(clear) {
+                for(var m = i; m > 0; m--) {
+                    for(var n = 0; n < gameData[0].length; n++) {
+                        gameData[m][n] = gameData[m - 1][n];
+                    };
+                };
+
+                for(var n = 0; n < gameData[0].length; n++) {
+                    gameData[0][n] = 0;
+                };
+
+                i++;
+            }
+        }
+    };
+
+    // 检查游戏结束
+    var checkGameOver = function() {
+        var gameOver = false;
+        for(var i = 0; i < gameData[0].length; i++) {
+            if(gameData[1][i] == 1) {
+                gameOver = true;
+            }
+        };
+        return gameOver;
+    };
+
+    // 使用下一个方块
+    var performNext = function(type, dir) {
+        cur = next;
+        setData();
+        next = SquareFactory.prototype.make(type, dir);
+        refreshDiv(gameData, gameDivs);
+        refreshDiv(next.data, nextDivs);
+    };
+
+    // 设置时间
+    var setTime = function(time) {
+        timeDiv.innerHTML = time;
+    };
+
     // 初始化方法
-    var init = function(doms) {
+    var init = function(doms, type, dir) {
         gameDiv = doms.gameDiv;
         nextDiv = doms.nextDiv;
-        cur = new Square();
-        next =new Square();
+        timeDiv = doms.timeDiv;
+        next = SquareFactory.prototype.make(type, dir);
         initDiv(gameDiv,gameData,gameDivs);
         initDiv(nextDiv,next.data,nextDivs);
-        cur.origin.x = 10;
-        cur.origin.y = 5;
-        setData();
-        refreshDiv(gameData,gameDivs);
         refreshDiv(next.data,nextDivs);
     }
 
     // 导出api
     this.init = init;
     this.down = down;
+    this.left = left;
+    this.right = right;
+    this.rotate = rotate;
+    this.fall = function() {
+        while(down());
+    };
+    this.performNext = performNext;
+    this.fixed = fixed;
+    this.checkClear = checkClear;
+    this.checkGameOver = checkGameOver;
+    this.setTime = setTime;
 }
